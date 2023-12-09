8dc29ab06ba5028bbd150660f5c6a48cd6b8813b
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-12-08 22:28:22 -0500

    installed dependencies on client side, all package.json files should now be complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,11 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 import './App.css';
 import { Outlet } from 'react-router-dom';
 
