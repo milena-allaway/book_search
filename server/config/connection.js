e7d058f9e07d3518f3ddfc9a2eeedd16f337f7b6
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-12-10 20:04:23 -0500

    testing connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,11 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 const mongoose = require("mongoose");
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
 );
 
 
